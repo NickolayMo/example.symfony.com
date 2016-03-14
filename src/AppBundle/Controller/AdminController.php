0497d0f100cc945a5f23196b7b9f8266f5b86ad9
Mokrenko Nickolay <kolya456@gmail.com>
2016-03-14 20:41:49 +0300

    Админка

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -5,39 +5,66 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Cart;
+use AppBundle\Entity\Game;
 use AppBundle\Entity\ShippingDetails;
 
 class AdminController extends Controller{
     
-    public function indexAction(Request $request, $category, $page){
+    public function indexAction(Request $request, $category, $page, $message){
         $paginator = $this->get('knp_paginator');        
         $em = $this->getDoctrine()->getManager();
         $games = $em->getRepository('AppBundle:Game')->getPaginatateGames($paginator, $category, $page);
         $games->setUsedRoute('admin_page');
         return $this->render('AppBundle:Admin:index.html.twig', array(
             'games'=>$games,
-            'category' => $category                       
+            'category' => $category,
+            'message'=>$message                       
                 
         ));
     }
     public function newAction(Request $request){
         $game = new Game();
-        $form->createForm('AppBundle\Form\GameType');
+        $form->createForm('AppBundle\Form\GameType', $game);
         $form->handleRequest($request);
         if ($form->isSubmited() && $form->isValid()) {
             $em = $this->getDoctrne()->getManager();
-            
-            
+            $em->persist($game);
+            $em->flash();
+            return $this->redirectToRoute('admin_game_index',['id'=>$game->getId()]);
         }
+        return $this->render('AppBundle:Admin:new.html.twig', [
+            'game'=>$game,
+            'form'=>$form->createView()
+            
+        ]);    
         
     }
     public function showAction(){
         
     }
-    public function deleteAction(){
-        
+    public function deleteAction(Request $request, Game $game){
+        $em = $this->getDoctrine()->getManager;
+        $em->remove(game);
+        $em->flush();   
+        return $this->redirectToRoute('admin_game_index', [
+            'message'=>'Игра '.$game.title.' была удалена'
+            ]);    
     }
-    public function editAction(){
+    public function editAction(Request $request, Game $game){
+        $form->createForm('AppBundle\Form\GameType', $game);
+        $form->handleRequest($request);
+        if ($form->isSubmited() && $form->isValid()) {
+            $em = $this->getDoctrne()->getManager();
+            $em->persist($game);
+            $em->flash();
+            return $this->redirectToRoute('admin_game_index', [
+            'message'=>'Игра '.$game.title.' была изменена'
+            ]);
+        }
+        return $this->render('AppBundle:Admin:edit.html.twig', [
+            'game'=>$game,
+            'form'=>$form->createView()            
+        ]);    
         
     }
     
